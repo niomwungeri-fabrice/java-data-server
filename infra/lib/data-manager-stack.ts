729dbeb7d0dfc3f003cfed3a7570f62121e07fca
Fabrice Niyomwungeri <niomwungeri.fabrice@gmail.com>
2024-11-07 17:08:25 -0700

    add actuator as helth endpoint

--- infra/lib/data-manager-stack.ts
+++ infra/lib/data-manager-stack.ts
@@ -1,30 +1,56 @@
 import * as cdk from 'aws-cdk-lib';
-import { Stack } from 'aws-cdk-lib';
-import { Vpc } from 'aws-cdk-lib/aws-ec2';
-import * as ecs from 'aws-cdk-lib/aws-ecs';
-import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
 import { Construct } from 'constructs';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
+import * as ecrAssets from 'aws-cdk-lib/aws-ecr-assets';
+import { Duration } from 'aws-cdk-lib';
 
-export class DataManagerAppStack extends Stack {
+export class DataManagerAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, stageName: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = Vpc.fromLookup(this, 'Vpc', { isDefault: true });
+    // use default
+    const vpc = ec2.Vpc.fromLookup(this, 'DefaultVpc', {
+      isDefault: true, // Look up the default VPC
+    });
 
-    // ECS Cluster and Fargate Service
-    const cluster = new ecs.Cluster(this, 'java-data-server-cluster', {
+    // Create an ECS cluster
+    const cluster = new ecs.Cluster(this, 'java-data-manager-cluster', {
       vpc,
     });
 
-    new ApplicationLoadBalancedFargateService(this, 'java-data-server-service', {
+    // Build Docker image from the `code` directory
+    const dockerImage = new ecrAssets.DockerImageAsset(this, 'java-data-manager-iocker-image', {
+      directory: '../code', // Reference the 'code' directory for the Dockerfile
+    });
+
+    // Define Fargate service with an Application Load Balancer
+    const fargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'java-data-manager-fargate-service', {
       cluster,
-      taskImageOptions: {
-        image: ecs.ContainerImage.fromAsset('../code'),  // Dockerfile in `code` folder
-        containerPort: 8080,
-        environment: { "stageName": stageName }
-      },
       desiredCount: 1,
-      publicLoadBalancer: true,
+      cpu: 256,
+      memoryLimitMiB: 512,
+      publicLoadBalancer: true, 
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromDockerImageAsset(dockerImage),
+        containerPort: 8081,
+        environment: {
+          'JAVA_OPTS': '-Xms512m -Xmx512m',
+          "stageName": stageName
+        }
+      },
+    });
+
+    // Configure health check on the /actuator endpoint
+    fargateService.targetGroup.configureHealthCheck({
+      path: '/actuator',
+      interval: Duration.seconds(30),
+    });
+
+    new cdk.CfnOutput(this, 'LoadBalancerDNS', {
+      value: fargateService.loadBalancer.loadBalancerDnsName,
+      description: 'DNS name of the load balancer for accessing the Spring Boot actuator endpoint',
     });
   }
 }
